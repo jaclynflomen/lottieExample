c2cf4c2c226db6b068aa7050c88328bc4cd6d63a
jaclynflomen <j_floman@fanshaweonline.ca>
2018-11-16 10:11:43 -0500

    base

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(() => {
+//get a ref to the lottie container
+const preloader = document.querySelector('.preloader');
+
+let preloadAnim = bodymovin.loadAnimation({
+    wrapper : preloader,
+    animType : 'svg',
+    loop : true,
+    autoplay : false,
+    path : './data/search.json'
+
+});
+
+function playAnimation () {
+    preloadAnim.play();
+};
+
+preloader.addEventListener("mouseover", playAnimation);
+
+})();
